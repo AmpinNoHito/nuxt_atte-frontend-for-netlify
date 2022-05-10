e3c21c90ddfc3b24cdd3c004acd702aa0b2d5bf3
AmpinNoHito <kazuma.yamamoto819@gmail.com>
2022-05-11 07:25:53 +0900

    baseURL changed

--- nuxt.config.js
+++ nuxt.config.js
@@ -48,7 +48,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'https://powerful-caverns-74192.herokuapp.com',
+    baseURL: 'https://app-atte-api.herokuapp.com',
     credentials: true,
   },
 
